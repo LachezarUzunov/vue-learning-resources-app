6e9fb9b614269e6ee1357217ea999ba9cf632e98
ВьLachezar Uzunov <lachezar.z.uzunov@gmail.com>
2024-11-29 11:17:20 +0200

    add: initial components

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul>
+        <stored-resources :resources="storedResources">
+        </stored-resources>
+    </ul>
+</template>
+
+<script>
+import StoredResources from '@/components/learning-resources/StoredResources.vue';
+    export default {
+        components: {
+            StoredResources
+        },
+        data() {
+            return {
+                storedResources: [
+                    {
+                        id: 'official-guide',
+                        title: 'Official Guide',
+                        description: 'The official Vue.js documentation',
+                        link: 'https://vuejs.org'
+                    },
+                    {
+                        id: 'google',
+                        title: 'Google',
+                        description: 'The official Vue.js documentation',
+                        link: 'https://vuejs.org'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
